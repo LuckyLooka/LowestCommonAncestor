5fb46e62db996a40d569fdc69a75a3699f007912
LuckyLooka <46965449+LuckyLooka@users.noreply.github.com>
2019-10-16 18:08:24 +0100

    Added testLcaStandardCase() Method
    -Added Unit Tests for standard findLCA calls on a binary tree.
    -Added Unit Test fail messages.

--- LCA/src/lca/LowestCommonAncestorTest.java
+++ LCA/src/lca/LowestCommonAncestorTest.java
@@ -7,9 +7,33 @@ import org.junit.jupiter.api.Test;
 
 class LowestCommonAncestorTest {
 
+	//	@Test
+//	void test() {
+//		
+//	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testLcaStandardCase() {
+		LcaSolution testTree = new LcaSolution();
+		testTree.root = new Node(1);
+		testTree.root.left = new Node(2);
+		testTree.root.right = new Node(3);
+		testTree.root.left.left = new Node(4);
+		testTree.root.left.right = new Node(5);
+		testTree.root.right.left = new Node(6);
+		testTree.root.right.right = new Node(7);
+		testTree.root.left.left.left = new Node(8);
+		testTree.root.left.left.right = new Node(9);
+		
+		assertEquals("LCA is incorrect, should be 4 but instead returned" +testTree.findLCA(8,9), 4, testTree.findLCA(8,9));
+		
+		assertEquals("LCA is incorrect, should be 1 but instead returned" +testTree.findLCA(7,9), 1, testTree.findLCA(7,9));
+		
+		assertEquals("LCA is incorrect, should be 3 but instead returned" +testTree.findLCA(6,7),3, testTree.findLCA(6,7));
+		
+		assertEquals("LCA is incorrect, should be 2 but instead returned" +testTree.findLCA(9,5), 2, testTree.findLCA(9,5));
+		
+		
 	}
 	
 	@Test
