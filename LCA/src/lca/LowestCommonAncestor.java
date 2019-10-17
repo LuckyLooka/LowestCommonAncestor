782e10c5c52e8a293f409481c7a6d27250469604
LuckyLooka <46965449+LuckyLooka@users.noreply.github.com>
2019-10-17 21:47:17 +0100

    Altered Lowest Common Ancestor Function
    Altered functions to support DAG as opposed to a binary tree

--- LCA/src/lca/LowestCommonAncestor.java
+++ LCA/src/lca/LowestCommonAncestor.java
@@ -3,20 +3,15 @@ package lca;
 import java.util.ArrayList; 
 import java.util.List;
 
-//Directed Acyclic Graph Data Structure
-class DAG {
-	   Node root; // assuming only one root exists
-
-	   class Node{
-	      List<Node> successors;
-	      List<Node> parents;
-	      int value; 
-	   }
-	}
+//Directed Acyclic Graph Node
+class Node{
+    List<Node> successors;
+    int value; 
+ }
 //Solution to LCA by recursively creating lists of the paths from root to nodes, and then comparing these lists
 //until unique nodes are reached and returning the node previous to those unique nodes as the LCA.
 class LowestCommonAncestor{
-	Node root; 
+	Node root;
     private List<Integer> path1 = new ArrayList<>(); 
     private List<Integer> path2 = new ArrayList<>(); 
   
@@ -24,12 +19,12 @@ class LowestCommonAncestor{
     int findLCA(int n1, int n2) { 
         path1.clear(); 
         path2.clear(); 
-        return findLCAInternal(root, n1, n2); 
+        return findLCADAG(root, n1, n2); 
     } 
   
-    private int findLCAInternal(Node root, int n1, int n2) { 
+    private int findLCADAG(Node root, int n1, int n2) { 
   
-    	//check to make sure both nodes are on the tree.
+    	//check to make sure both nodes are on the graph.
         if(!findPath(root, n1, path1)) {
         		System.out.println("no path to n1");
         	return -1;
@@ -40,21 +35,31 @@ class LowestCommonAncestor{
         }
         
         
-        int i;
-        int shortestPath;
-        if (path1.size() > path2.size())
-        	shortestPath = path2.size();
-        else
-        	shortestPath = path1.size();
+//        int i;
+//        int shortestPath;
+//        if (path1.size() > path2.size())
+//        	shortestPath = path2.size();
+//        else
+//        	shortestPath = path1.size();
+//        
+//        for (i = 0; i < shortestPath; i++) { 
+//              
+//         //System.out.println(path1.get(i) + " " + path2.get(i)); 
+//            if (!path1.get(i).equals(path2.get(i))) 
+//                break; 
+//        } 
+//  
+//        return path1.get(i-1); 
         
-        for (i = 0; i < shortestPath; i++) { 
-              
-         //System.out.println(path1.get(i) + " " + path2.get(i)); 
-            if (!path1.get(i).equals(path2.get(i))) 
-                break; 
-        } 
-  
-        return path1.get(i-1); 
+        int endPath1 = path1.size()-1;
+        int endPath2 = path2.size()-1;
+        
+        while(endPath1 > -1 && endPath2 > -1) {
+        	if (path1.get(endPath1).equals(path2.get(endPath2))) {
+        		return path1.get(endPath1-1);
+        	}
+        }
+        return -1;
     } 
       
     // Finds the path from root node to given node of the tree, Stores the 
@@ -68,19 +73,17 @@ class LowestCommonAncestor{
           
         // Store this node . The node will be removed if 
         // not in path from root to n. 
-        path.add(root.data); 
+        path.add(root.value); 
   
-        if (root.data == n) { 
+        if (root.value == n) { 
             return true; 
         } 
-  
-        if (root.left != null && findPath(root.left, n, path)) { 
-            return true; 
-        } 
-  
-        if (root.right != null && findPath(root.right, n, path)) { 
-            return true; 
-        } 
+        
+        //Recursively creates a single path from root to a given node.
+        for(int i = 0; i<root.successors.size(); i++) {
+        	if(root.successors.get(i)!=null && findPath(root.successors.get(i), n, path))
+        		return true;
+        }
   
         // If not present in subtree rooted with root, remove root from 
         // path[] and return false 
@@ -88,30 +91,6 @@ class LowestCommonAncestor{
   
         return false; 
     } 
-    
-    //Attempt to display binary tree in console, gave up as it is unnecessary waste of time.
-//	public void displayTree(Node root) {
-//		Node current = root;
-//		Node temp = current;
-//		System.out.print("(" +current.data +")");
-//		//Right Wing
-//		while(current!=null) {
-//			current = current.right;
-//			System.out.print(" -- (" +temp.data +")");
-//		}
-//		current = temp;
-//		if (current.left!=null)
-//			System.out.println(" |");
-//		while(temp != null) {
-//			if (temp.right != null) {
-//				temp = temp.right;
-//				if (temp.left!=null)
-//					System.out.print("      \\");
-//			}
-//						
-//		}
-//		
-//	}
 }
 
 
